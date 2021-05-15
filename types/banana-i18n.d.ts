a4e2f20f60ed18ac461cf55f801555d521694bb7
Siddharth VP <siddharthvp@gmail.com>
2021-05-15 18:13:49 +0530

    updates from upstream PRs: types and build

---
+++ types/banana-i18n.d.ts
@@ -0,0 +1,29 @@
+export interface Messages {
+	[ messageKey: string ]: string|Record<string, any>;
+}
+
+export interface BananaOptions {
+	messages?: Messages | MessageSource;
+	finalFallback?: string;
+	wikilinks?: boolean;
+}
+
+export interface BananaConstructor {
+	new ( locale: string, options?: BananaOptions ): Banana;
+}
+
+export type MessageSource = Record<string, Messages>;
+
+export type ParameterType = string|object|number|undefined;
+
+export interface Banana {
+	locale: string;
+	load( messageSource: Messages | MessageSource, locale?: string ): void;
+	i18n( key: string, ...params: ParameterType[] ): string;
+	setLocale( locale: string ): void;
+	getFallbackLocales(): string[];
+	getMessage( messageKey: string ): string;
+	registerParserPlugin( name: string, plugin: ((nodes: ParameterType[]) => string) ): void;
+}
+
+export const Banana: BananaConstructor;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,59 +1,10 @@
-export default class Banana {
-	constructor(locale: string, options?: BananaOptions);
-	locale: string;
-	parser: BananaParser;
-	messageStore: BananaMessageStore;
-	finalFallback: string;
+import { Banana } from './banana-i18n';
 
-	load(messageSource: MessageSource): void;
-	load(messageSource: Messages, locale: string): void;
-	i18n(key: string, ...params: ParameterType[]): string;
-	setLocale(locale: string): void;
-	getFallbackLocales(): string[];
-	getMessage(messageKey: string): string;
-	registerParserPlugin(name: string, plugin: Function)
-}
+export default Banana;
 
-export interface BananaOptions {
-	messages?: Messages;
-	finalFallback?: string;
-}
-
-export interface Messages {
-	[messageKey: string]: string;
-}
-
-export interface MessageSource {
-	[localizer: string]: Messages;
-}
-
-export type ParameterType = string | object | number | undefined;
-
-export class BananaParser {
-	constructor (locale: string, options?: { wikilinks: boolean });
-	locale: string;
-	wikilinks: boolean;
-	emitter: BananaEmitter;
-}
-
-export class BananaEmitter {
-	constructor(locale: string);
-	emit(node: any, replacements: any): any;
-	concat(nodes: any): string;
-	replace (nodes: any, replacements: any): string;
-	plural(nodes: any): string;
-	gender(nodes: any): string;
-	grammar(nodes: any): string;
-	wikilink(nodes: any): string;
-	extlink(nodes: any): string;
-	bidi(nodes: any): string;
-	formatnum(nodes: any): string;
-}
-
-export class BananaMessageStore {
-	constructor();
-	load(messageSource: MessageSource): void;
-	load(messageSource: Messages, locale: string): void;
-	getMessage(key: string, locale: string): string;
-	hasLocale(locale: string): boolean;
-}
+export {
+	BananaOptions,
+	Messages,
+	MessageSource,
+	ParameterType,
+} from './banana-i18n';
